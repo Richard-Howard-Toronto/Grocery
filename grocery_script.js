6d1405d88da44191ca758baa7548768be8041929
Richard-Howard-Toronto <rh_gk@rogers.com>
2018-10-21 16:10:22 -0400

    test

--- grocery_script.js
+++ grocery_script.js
@@ -1,63 +1,108 @@
-$(function() {
-  $.getJSON('petfinder_date.json')
-    .then((result) => {
 
-      const petsArray = result.petfinder.pets.pet;
+  // first we create an app namespace
+  var app = {};
 
-      console.log(petsArray);
+  app.addItem = function(itemName){
+    // 1. write the code to create a list item and add it to the groceries list
+    // You can test it from the browser by running app.addItem('Carrots');
+    const item = `<li>${itemName}<button data-purchase>Delete</button></li>`;
+    $('ul.groceries').append(item);
 
+  };
 
-      const RabbitArray = petsArray.map((animal) => {
-        return {
-          name: animal.name.$t,
-          age: animal.age.$t,
-          size: animal.size.$t,
-          type: animal.animal.$t
-        }
+  app.addHeading = function(headingName) {
+    // 2. write the code to create a list item header and add it to the groceries list
+    // You can test it from the browser by running app.addHeading('Vegetables');
+    const item = `<li class="heading">${headingName }</li>`
+    $('ul.groceries').append(item);
+  };
 
-      });
-      console.log(RabbitArray);
+  app.addCalories = function(calories) {
+    if (calories>0) {
+        console.log(`there are ${calories} calories`);
+        msg = `there are ${calories} calories`;
+    } else if (calories=0) {
+        console.log(`no calories, you bought water?`);
+        msg = `no calories, you bought water?`;
+    } else {
+        console.log(`no food has negative calories`);
+        msg = `no calories, you bought water?`;
+    }
+    const calories_item = `<li class="heading">${msg }</li>`
+    $('ul.groceries').append(calories_item);
+  };
 
-      const noDogs = petsArray.filter(pet => {
-
-        const isNotDog = pet.animal.$t !== "Dog";
-        return isNotDog;
-
-      }).map(pet => {
-        const name = pet.name.$t;
-        return name;
-      });
-
-      console.log(noDogs);
-
-
-
-      const noCats = petsArray.filter(pet => {
-
-        const isNotCat = pet.animal.$t !== "Cat";
-        return isNotCat;
-
-      }).map(pet => {
-        const name = pet.name.$t;
-        return name;
-      });
-
-      console.log(noCats);
+  app.addCalories(10);
 
 
-      const Rabbit = petsArray.filter(pet => {
+  app.init = function() {
+    // listen for the "submit" event on the form.
+    $('form').on('submit', function(e) {
 
-        const isRabbit = pet.animal.$t == "Rabbit";
-        return isRabbit;
+      // 3. stop the form from submitting and refreshing the page
+      e.preventDefault();
 
-      }).map(pet => {
-        const name = pet.name.$t;
-        return name;
-      });
+      // 4. Do stuff to make it go into the form
+      const $formInput = $('input[name="itemName"]');
+      const itemValue = $formInput.val();
+      const $checkbox = $('#heading:checked');
 
-      console.log(Rabbit);
+      if ($checkbox.length >=0){
+        app.addHeading(itemValue)
+      } else {
+        app.addItem(itemValue);
+      }
 
-
+      $formInput.val('');
 
     });
-});
+
+    // 5. make it sortable with jQuery UI
+    // https://jqueryui.com/sortable/#connect-lists
+
+    $('[data-list], [data-in-cart]').sortable({
+      connectWith: '[data-groceries]'
+    })
+
+    //Create listener for delete button
+
+    $('.wrapper').on('click', function(){
+
+      var newEl = document.createElement('li');
+      var newEl2 = document.createElement('li');
+
+
+      // Create a text node and store it in a variable.
+
+      var newText = document.createTextNode(    $('[data-list], [data-in-cart]'));
+
+
+
+      // Attach the new text node to the new element.
+
+      newEl.appendChild(newText);
+
+
+      // insert the purchased item at the top of the list
+
+      var position = document.getElementsByTagName('ol')[0];
+
+
+      // Insert the new element into its position.
+
+      position.appendChild(newEl);
+
+
+        console.log('hi, ...');
+    })
+
+        // $('ul.got').append('xxxxx');
+
+  };
+
+
+
+  $(function() {
+    // on document ready we run app.init
+    app.init();
+  });
